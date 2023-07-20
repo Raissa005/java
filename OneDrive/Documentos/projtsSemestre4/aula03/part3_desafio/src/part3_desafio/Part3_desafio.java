fbe9e2b9b059a7337f1a6952e22a93fed50f40ea
Raíssa <leiteraissavitoria@gmail.com>
2023-07-20 18:49:15 -0300

    Desafio bobbleshort

---
+++ OneDrive/Documentos/projtsSemestre4/aula03/part3_desafio/src/part3_desafio/Part3_desafio.java
@@ -0,0 +1,55 @@
+package part3_desafio;
+
+public class Part3_desafio {
+  
+    public static void main(String[] args) {
+        double[] vetor = {12, 11, 17, 54.5, 54.3, 37, 74, 97};
+        double[] vetor2 = {3, 4, 7, 13, 9};
+        
+        ordenar(vetor, true);
+        print(vetor);
+        ordenar(vetor, false);
+        print(vetor);
+       
+        ordenar(vetor2, true);
+        print(vetor2);
+        ordenar(vetor2, false);
+        print(vetor2);
+    }
+
+    static void ordenar(double[] vet, boolean cresc) {
+        if(cresc ){
+            System.out.println("Vetor em ordem crescente: ");
+        }else{
+            System.out.println("Vetor em ordem decrescente: ");
+        }
+        double aux;
+        int n = vet.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (cresc) {
+                    if (vet[j] > vet[j + 1]) {
+                        aux = vet[j];
+                        vet[j] = vet[j + 1];
+                        vet[j + 1] = aux;		
+                    }
+                } else {
+                    if (vet[j] < vet[j + 1]) {
+                        aux = vet[j];
+                        vet[j] = vet[j + 1];
+                        vet[j + 1] = aux;		
+                    }
+                }
+            }
+        }
+    }
+    
+    static void print(double[] vet) {
+        for (double num : vet) {
+            System.out.print(num + " ");
+        }       
+          System.out.println("");
+    }
+}
+
+// ordenar crescente depois decrescente (boolean)
